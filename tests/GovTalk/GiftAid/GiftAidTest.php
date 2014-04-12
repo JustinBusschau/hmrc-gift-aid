d8cc9dd7730ccd6d80e6b30f48eafb9e9ae9c446
Justin Busschau <jbusschau@churchworks.com>
2014-04-12 01:07:05 +0100

    Copyright message

--- tests/GovTalk/GiftAid/GiftAidTest.php
+++ tests/GovTalk/GiftAid/GiftAidTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the GovTalk\GiftAid package
  *
- * (c) By The People Ltd.
+ * (c) Justin Busschau
  *
  * For the full copyright and license information, please see the LICENSE
  * file that was distributed with this source code.
