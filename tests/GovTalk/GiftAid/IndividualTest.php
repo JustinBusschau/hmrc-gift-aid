aa1adbe06c4ac03efdd6d3e3741a88ffc6b0e947
Justin Busschau <justin.busschau@gmail.com>
2015-04-12 01:31:28 +0100

    cater for foreign addresses

--- tests/GovTalk/GiftAid/IndividualTest.php
+++ tests/GovTalk/GiftAid/IndividualTest.php
@@ -21,7 +21,7 @@ class IndividualTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        
+
         $this->individual = new Individual(
             'Mr',
             'Rex',
@@ -30,8 +30,18 @@ class IndividualTest extends TestCase
             '3',
             'SW1A 1AA'
         );
+
+        $this->foreign = new Individual(
+            'Ds',
+            'Johannes',
+            'Doper',
+            '011 452 1256',
+            '27',
+            '',
+            true
+        );
     }
-    
+
     public function testIndividualCreation()
     {
         $this->assertEquals($this->individual->getTitle(), 'Mr');
@@ -40,8 +50,20 @@ class IndividualTest extends TestCase
         $this->assertEquals($this->individual->getPhone(), '077 1234 5678');
         $this->assertEquals($this->individual->getHouseNum(), '3');
         $this->assertEquals($this->individual->getPostcode(), 'SW1A 1AA');
+        $this->assertEquals($this->individual->getIsOverseas(), 'no');
     }
-    
+
+    public function testForeignIndividualCreation()
+    {
+        $this->assertEquals($this->foreign->getTitle(), 'Ds');
+        $this->assertEquals($this->foreign->getSurname(), 'Doper');
+        $this->assertEquals($this->foreign->getForename(), 'Johannes');
+        $this->assertEquals($this->foreign->getPhone(), '011 452 1256');
+        $this->assertEquals($this->foreign->getHouseNum(), '27');
+        $this->assertEquals($this->foreign->getPostcode(), '');
+        $this->assertEquals($this->foreign->getIsOverseas(), 'yes');
+    }
+
     public function testUpdateIndividual()
     {
         $this->individual->setTitle('Mrs');
@@ -50,6 +72,7 @@ class IndividualTest extends TestCase
         $this->individual->setPhone('020 8765 4321');
         $this->individual->setHouseNum('2');
         $this->individual->setPostcode('NW1A 1AA');
+        $this->individual->setIsOverseas(false);
 
         $this->assertEquals($this->individual->getTitle(), 'Mrs');
         $this->assertEquals($this->individual->getSurname(), 'Malady');
@@ -57,5 +80,6 @@ class IndividualTest extends TestCase
         $this->assertEquals($this->individual->getPhone(), '020 8765 4321');
         $this->assertEquals($this->individual->getHouseNum(), '2');
         $this->assertEquals($this->individual->getPostcode(), 'NW1A 1AA');
+        $this->assertEquals($this->individual->getIsOverseas(), 'no');
     }
 }
